3ee96a3d5e0ba8625ef503a5ebfb5ba642060dc2
feliciteuwase <felciteuwase98@gmail.com>
2023-03-20 01:30:12 -0400

    added change

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Welcome to web dev </h1>
+        <h2>This is branch for assignments 2</h2>
+
         <a
           className="App-link"
           href="https://reactjs.org"
